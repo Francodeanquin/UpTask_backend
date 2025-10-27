7b4a27ca851c1e224364f548ec507f6cbbd77e00
Franco Otta <franco.otta@awsoft.com.ar>
2025-10-27 19:01:50 -0300

    modificacion cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,16 +1,17 @@
+// src/config/cors.ts
 import { CorsOptions } from "cors";
+
+const whitelist = [process.env.FRONTEND_URL];
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-  
-    const whitelist = [process.env.FRONTEND_URL];
-    if (process.argv) {
-      whitelist.push(undefined);
-    }
-    if (whitelist.includes(origin)) {
+    // Permitimos sin origin solo en desarrollo local
+    if (!origin || whitelist.includes(origin)) {
       callback(null, true);
     } else {
+      console.log("❌ Bloqueado por CORS:", origin);
       callback(new Error("Error de CORS"));
     }
   },
+  credentials: true, // si usás cookies, tokens, etc.
 };
