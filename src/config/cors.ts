cff0a9384508fa95edbc3face6fec8c9572eb770
Franco Otta <franco.otta@awsoft.com.ar>
2025-10-27 19:14:40 -0300

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,11 +1,13 @@
-// src/config/cors.ts
 import { CorsOptions } from "cors";
 
-const whitelist = [process.env.FRONTEND_URL];
+const whitelist = [
+  process.env.FRONTEND_URL, // deploy
+  "http://localhost:5173", // local
+];
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-    // Permitimos sin origin solo en desarrollo local
+    // Permitimos requests sin origin (Postman, fetch server-to-server)
     if (!origin || whitelist.includes(origin)) {
       callback(null, true);
     } else {
@@ -13,5 +15,5 @@ export const corsConfig: CorsOptions = {
       callback(new Error("Error de CORS"));
     }
   },
-  credentials: true, // si usás cookies, tokens, etc.
+  credentials: true, // si usás cookies o sessions
 };
